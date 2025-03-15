630e2e11a4154193b3e883ab4663b9177a6516ef
AdiByte30 <adityaandhale30@gmail.com>
2025-03-15 10:50:02 +0530

    All names changed to svara and errors resolved

--- lib/svara_flutter_sdk.dart
+++ lib/svara_flutter_sdk.dart
@@ -1,19 +1,16 @@
 /// Svara Flutter SDK
 
-/// This package provides functionalities for Avion
+/// This package provides functionalities for Svara
 /// Author: Arpa
 /// Version: 1.0.0
 
 library;
 
-// Export avion module
+// Export svara module
 export 'src/svara_collection.dart';
 export 'src/svara_event_handler.dart';
 export 'src/svara_services.dart';
 export 'src/svara_user_data.dart';
-
-// Export mediasfu module (wrapper)
-//export 'src/mediasfu/mediasoup_client.dart';
 
 // Export logging utilities
 export 'logging_interceptor.dart';
